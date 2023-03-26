4db45bef529bc2d50f367c6d08a8368de3cc0462
friedeggs01 <nhokngok.online14203@gmail.com>
2023-03-26 13:18:25 +0700

    m

---
+++ 115A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> v[10001];
+int mx = -1;
+void dfs(int x, int level){
+    mx = max(mx, level);
+    for(int i=0; i<v[x].size(); i++){
+        dfs(v[x][i], level+1);
+    }
+}
+
+int main() {
+    int n, x;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> x;
+        if(x!=-1){
+            v[x].push_back(i);
+        } else {
+            v[0].push_back(i);
+        }
+    }
+    for(int i=0; i<v[0].size(); i++){
+        dfs(v[0][i], 1);
+    }
+    cout << mx;
+}
